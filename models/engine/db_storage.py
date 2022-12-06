8114516d095a79530649e3a59de6ac01d72156c3
Rim El Filali <rimo1902@gmail.com>
2022-12-06 21:11:52 +0000

    add

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -12,12 +12,59 @@ from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
+
 
 class DBStorage:
     """DBStorage Class"""
     __engine = None
     __session = None
-    classes = ["User", "State", "City", "Amenity", "Place", "Review"]
-    
+
     def __init__(self):
         """Instantiate a DBStorage object"""
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = getenv('HBNB_ENV')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+                                      HBNB_MYSQL_USER, HBNB_MYSQL_PWD,
+                                      HBNB_MYSQL_HOST, HBNB_MYSQL_DB))
+        
+        if HBNB_ENV == "test":
+            Base.metadata.drop_all(self.__engine)
+    
+    def all(self, cls=None):
+        """query on the current database session
+        all objects depending of the class name
+        """
+        new_dict = {}
+        for a in classes:
+            if cls is None or cls is classes[a] or cls is a:
+                objects = self.__session.query(classes[a]).all()
+                for obj in objects:
+                    k = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[k] = obj
+        return (new_dict)
+
+     def new(self, obj):
+         """add the object to the current database session"""
+         self.__session.add(obj)
+
+     def save(self):
+         """commit all changes of the current database session"""
+         self.__session.commit()
+
+     def delete(self, obj=None):
+         """delete from the current database session obj if not None"""
+         if obj is not None:
+             self.__session.delete(obj)
+
+     def reload(self):
+         """reload the database"""
+         Base.metadata.create_all(self.__engine)
+         self.__session = scoped_session(sessionmaker(bind=self.__engine))
+
+     def close(self):
+         self.__session.remove()
