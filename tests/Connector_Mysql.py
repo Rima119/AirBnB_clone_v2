3af7ed75e17f505ae3718751490a57d71458e6c1
Rim El Filali <rimo1902@gmail.com>
2022-12-07 07:38:14 -0800

    update

--- tests/Connector_Mysql.py
+++
@@ -1,8 +0,0 @@
-import MySQLdb
-"""
-Test if we create a new state , it's saves to the record
-"""
-
-db = MySQLdb.connect(host="localhost", user="root", passwd="root", db="db_states", charset="utf8")
-cur = db.cursor()
-cur.execute("USE db_states")
