05f285306d565414ae44e33bad070f4fd4aaddc2
Rim El Filali <rimo1902@gmail.com>
2023-01-16 10:10:54 +0000

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """
-script that starts a Flask web application:
+Script that starts a Flask web application:
 web application must be listening on 0.0.0.0, port 5000
-Routes:
-    /states_list: display a HTML page: (inside the tag BODY)
 """
 
 from models import storage
@@ -15,14 +13,14 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """ display a HTML page (inside the tag BODY) """
+    """ Display a HTML page (inside the tag BODY) """
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    """ closes the storage on teardown """
+    """ Closes the storage on teardown """
     storage.close()
 
 
