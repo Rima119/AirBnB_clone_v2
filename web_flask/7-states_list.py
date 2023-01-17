03ce4e3c549bcdc678a486342edc2fc83310e56a
Rim El Filali <rimo1902@gmail.com>
2023-01-17 12:34:48 -0800

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """
-Script that starts a Flask web application:
-web application must be listening on 0.0.0.0, port 5000
+Script that starts a Flask web application
 """
-
 from models import storage
 from models.state import State
 from flask import Flask, render_template
@@ -13,17 +11,16 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """ Display a HTML page (inside the tag BODY) """
+    """Display a HTML page (inside the tag BODY)"""
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    """ Closes the storage on teardown """
+    """Closes the storage on teardown"""
     storage.close()
 
 
 if __name__ == '__main__':
-    """Main function"""
     app.run(host='0.0.0.0', port='5000')
